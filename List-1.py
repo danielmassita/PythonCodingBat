123aa8df62dd340681a477132eb328ef7409b8de
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-04-25 23:28:51 -0300

    Update List-1.py

--- List-1.py
+++ List-1.py
@@ -8,5 +8,25 @@ Basic python list problems -- no loops.. Use a[0], a[1], ... to access elements 
  middle_way	         make_ends              	 has23
  """
 
+"""
+List-1 > first_last6
+prev  |  next  |  chance
+Given an array of ints, return True if 6 appears as either the first or last element in the array. The array will be length 1 or more.
 
 
+first_last6([1, 2, 6]) → True
+first_last6([6, 1, 2, 3]) → True
+first_last6([13, 6, 1, 2, 3]) → False
+
+Hint:
+The first element is nums[0] and the last element is nums[len(nums) - 1]. Check if either of those is == 6.
+"""
+def first_last6(nums):
+  if nums[0] == 6 or nums[-1] == 6:
+    return True
+  else:
+    return False
+
+def first_last6(nums):
+  return (nums[0] == 6) or (nums[-1] == 6)
+  
