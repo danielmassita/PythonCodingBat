24ce2ebd773ee8cbc75d95490be5b1f85cb0ce5c
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-04-25 23:24:45 -0300

    Create List-1.py

---
+++ List-1.py
@@ -0,0 +1,12 @@
+"""
+List-1 chance
+Basic python list problems -- no loops.. Use a[0], a[1], ... to access elements in a list, len(a) is the length.
+
+ first_last6 H    	 same_first_last H      	 make_pi
+ common_end	         sum3                   	 rotate_left3
+ reverse3	           max_end3                	 sum2
+ middle_way	         make_ends              	 has23
+ """
+
+
+
