f8adfa1a3778a64b6ce9e3fd712b4f9db96defaa
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-04-26 22:48:18 -0300

    Update List-1.py

--- List-1.py
+++ List-1.py
@@ -95,12 +95,56 @@ OK
 All Correct
 """
   
+"""
+List-1 > make_pi
+prev  |  next  |  chance
+Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
+
+
+make_pi() → [3, 1, 4]
+"""
+
+def make_pi():
+ pi = [3, 1, 4]
+ return pi
+
+"""
+Expected	Run		
+make_pi() → [3, 1, 4]	[3, 1, 4]	OK	
+
+All Correct
+"""
   
+"""
+List-1 > common_end
+prev  |  next  |  chance
+Given 2 arrays of ints, a and b, return True if they have the same first element or they have the same last element. Both arrays will be length 1 or more.
+
+
+common_end([1, 2, 3], [7, 3]) → True
+common_end([1, 2, 3], [7, 3, 2]) → False
+common_end([1, 2, 3], [1, 3]) → True
+"""
+
+def common_end(a, b):
+ return (a[0] == b[0]) or (a[-1] == b[-1])
   
-  
-  
-  
-  
-  
-  
+def common_end(a, b):
+  if a[0] == b[0] or a[-1] == b[-1]:
+    return True
+  else:
+    return False
+
+"""
+  Expected	Run		
+common_end([1, 2, 3], [7, 3]) → True	True	OK	
+common_end([1, 2, 3], [7, 3, 2]) → False	False	OK	
+common_end([1, 2, 3], [1, 3]) → True	True	OK	
+common_end([1, 2, 3], [1]) → True	True	OK	
+common_end([1, 2, 3], [2]) → False	False	OK	
+other tests
+OK	
+
+All Correct
+"""  
   
