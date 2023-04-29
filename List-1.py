e9bf6aa50d06be9e3312ae2a32026e90e9369e35
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-04-28 22:09:21 -0300

    Update List-1.py

--- List-1.py
+++ List-1.py
@@ -174,3 +174,31 @@ sum3([2, 7, 2]) → 11	11	OK
 All Correct
 """
 
+"""
+List-1 > rotate_left3
+prev  |  next  |  chance
+Given an array of ints length 3, return an array with the elements "rotated left" so {1, 2, 3} yields {2, 3, 1}.
+
+
+rotate_left3([1, 2, 3]) → [2, 3, 1]
+rotate_left3([5, 11, 9]) → [11, 9, 5]
+rotate_left3([7, 0, 0]) → [0, 0, 7]
+"""
+
+def rotate_left3(nums):
+ nums.append(nums[0])
+ return nums[1:4]
+
+"""
+Expected	Run		
+rotate_left3([1, 2, 3]) → [2, 3, 1]	[2, 3, 1]	OK	
+rotate_left3([5, 11, 9]) → [11, 9, 5]	[11, 9, 5]	OK	
+rotate_left3([7, 0, 0]) → [0, 0, 7]	[0, 0, 7]	OK	
+rotate_left3([1, 2, 1]) → [2, 1, 1]	[2, 1, 1]	OK	
+rotate_left3([0, 0, 1]) → [0, 1, 0]	[0, 1, 0]	OK	
+other tests
+OK	
+
+All Correct
+"""
+
