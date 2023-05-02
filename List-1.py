a0c0d17443deec0dcba8eb1f930ee98ac464f2fa
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-05-01 23:19:42 -0300

    Update List-1.py

--- List-1.py
+++ List-1.py
@@ -371,3 +371,36 @@ OK
 
 All Correct
 """
+
+"""
+List-1 > make_ends
+prev  |  next  |  chance
+Given an array of ints, return a new array length 2 containing the first and last elements from the original array. The original array will be length 1 or more.
+
+
+make_ends([1, 2, 3]) → [1, 3]
+make_ends([1, 2, 3, 4]) → [1, 4]
+make_ends([7, 4, 6, 2]) → [7, 2]
+"""
+
+def make_ends(nums):
+ PrimeiroElemento = nums[0] 
+ SegundoElemento = nums[-1]
+ NovaLista = [PrimeiroElemento, SegundoElemento]
+ return NovaLista
+
+"""
+Expected	Run		
+make_ends([1, 2, 3]) → [1, 3]	[1, 3]	OK	
+make_ends([1, 2, 3, 4]) → [1, 4]	[1, 4]	OK	
+make_ends([7, 4, 6, 2]) → [7, 2]	[7, 2]	OK	
+make_ends([1, 2, 2, 2, 2, 2, 2, 3]) → [1, 3]	[1, 3]	OK	
+make_ends([7, 4]) → [7, 4]	[7, 4]	OK	
+make_ends([7]) → [7, 7]	[7, 7]	OK	
+make_ends([5, 2, 9]) → [5, 9]	[5, 9]	OK	
+make_ends([2, 3, 4, 1]) → [2, 1]	[2, 1]	OK	
+other tests
+OK	
+
+All Correct
+"""
