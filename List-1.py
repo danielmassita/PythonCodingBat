27b61302a0e8ee1421f32070c109ea76fa6eef68
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-05-01 23:13:53 -0300

    Update List-1.py

--- List-1.py
+++ List-1.py
@@ -297,3 +297,47 @@ OK
 
 All Correct
 """
+
+"""
+List-1 > sum2
+prev  |  next  |  chance
+Given an array of ints, return the sum of the first 2 elements in the array. If the array length is less than 2, just sum up the elements that exist, returning 0 if the array is length 0.
+
+
+sum2([1, 2, 3]) → 3
+sum2([1, 1]) → 2
+sum2([1, 1, 1, 1]) → 2
+"""
+
+def sum2(nums):
+  tamanho = len(nums)
+  if tamanho == 0:
+    return 0
+  elif tamanho == 1:
+    return nums[0]
+  else:
+    return nums[0]+nums[1]
+
+def sum2(nums):
+  if len(nums) == 0:
+    return 0
+  elif len(nums) == 1:
+    return nums[0]
+  else:
+    return nums[0]+nums[1]
+   
+"""
+Expected	Run		
+sum2([1, 2, 3]) → 3	3	OK	
+sum2([1, 1]) → 2	2	OK	
+sum2([1, 1, 1, 1]) → 2	2	OK	
+sum2([1, 2]) → 3	3	OK	
+sum2([1]) → 1	1	OK	
+sum2([]) → 0	0	OK	
+sum2([4, 5, 6]) → 9	9	OK	
+sum2([4]) → 4	4	OK	
+other tests
+OK	
+
+All Correct
+"""
