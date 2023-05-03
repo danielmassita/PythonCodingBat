d3fd58fba5ca70f76be06930c838ed8156e52c55
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-05-02 23:44:45 -0300

    Create Warmup-2.py

---
+++ Warmup-2.py
@@ -0,0 +1,9 @@
+"""
+Warmup-2 chance
+Medium warmup string/list problems with loops (solutions available)
+
+ string_times H   	   front_times H	    string_bits H
+ string_splosion H  	 last2 H	          array_count9 H
+ array_front9 H	       array123 H   	    string_match H
+ """
+
