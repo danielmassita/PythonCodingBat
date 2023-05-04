1213a9a17eeaa20a69615b4def87d1b9619e5a5a
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-05-04 11:40:36 -0300

    Update Warmup-2.py

--- Warmup-2.py
+++ Warmup-2.py
@@ -53,3 +53,39 @@ string_times('code', 3) → 'codecodecode'	'codecodecode'	OK
 All Correct
 next  |  chance
 """
+
+"""
+Warmup-2 > front_times
+prev  |  next  |  chance
+Given a string and a non-negative int n, we'll say that the front of the string is the first 3 chars, or whatever is there if the string is less than length 3. Return n copies of the front;
+
+
+front_times('Chocolate', 2) → 'ChoCho'
+front_times('Chocolate', 3) → 'ChoChoCho'
+front_times('Abc', 3) → 'AbcAbcAbc'
+"""
+
+def front_times(str, n):  # str é a string (pode ser vazia? talvez... entender str = ""); n é o número não negativo (pode ser zero? talvez...)
+  result = ""
+  front = str[0:3] # chars de zero, um, dois - três EXcluso...
+  for i in range(n):
+    result += front
+  return result
+
+def front_times(str, n): # Resposta alternativa, usando uma sintaxe mais pythonic str[0:3]*n, mas precisa ver casos menores que len(str) < 3... 
+  if len(str) < 3:
+    return str*n
+  else:
+    return str[0:3]*n
+   
+# Our Solution:
+def front_times(str, n):
+  front_len = 3
+  if front_len > len(str):
+    front_len = len(str)
+  front = str[:front_len]
+  
+  result = ""
+  for i in range(n):
+    result = result + front
+  return result
