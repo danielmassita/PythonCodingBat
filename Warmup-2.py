98bf674be1151ab152aad563d84231260a8ee97b
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-05-03 23:47:38 -0300

    Update Warmup-2.py

--- Warmup-2.py
+++ Warmup-2.py
@@ -30,7 +30,26 @@ def string_times(str, n):
   else:
     return str*n
 
-   
-"""
+# Our Solution:
+def string_times(str, n):
+  result = ""
+  for i in range(n):  # range(n) is [0, 1, 2, .... n-1]
+    result = result + str  # could use += here
+  return result   
 
+"""
+Expected	Run		
+string_times('Hi', 2) → 'HiHi'	'HiHi'	OK	
+string_times('Hi', 3) → 'HiHiHi'	'HiHiHi'	OK	
+string_times('Hi', 1) → 'Hi'	'Hi'	OK	
+string_times('Hi', 0) → ''	''	OK	
+string_times('Hi', 5) → 'HiHiHiHiHi'	'HiHiHiHiHi'	OK	
+string_times('Oh Boy!', 2) → 'Oh Boy!Oh Boy!'	'Oh Boy!Oh Boy!'	OK	
+string_times('x', 4) → 'xxxx'	'xxxx'	OK	
+string_times('', 4) → ''	''	OK	
+string_times('code', 2) → 'codecode'	'codecode'	OK	
+string_times('code', 3) → 'codecodecode'	'codecodecode'	OK	
+
+All Correct
+next  |  chance
 """
