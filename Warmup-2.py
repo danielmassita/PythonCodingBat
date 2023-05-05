ad4602f5d443302c55a8d79317469ba2e06b2a1c
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-05-05 01:56:35 -0300

    Update Warmup-2.py

--- Warmup-2.py
+++ Warmup-2.py
@@ -89,3 +89,59 @@ def front_times(str, n):
   for i in range(n):
     result = result + front
   return result
+
+ """
+ Expected	Run		
+front_times('Chocolate', 2) → 'ChoCho'	'ChoCho'	OK	
+front_times('Chocolate', 3) → 'ChoChoCho'	'ChoChoCho'	OK	
+front_times('Abc', 3) → 'AbcAbcAbc'	'AbcAbcAbc'	OK	
+front_times('Ab', 4) → 'AbAbAbAb'	'AbAbAbAb'	OK	
+front_times('A', 4) → 'AAAA'	'AAAA'	OK	
+front_times('', 4) → ''	''	OK	
+front_times('Abc', 0) → ''	''	OK	
+
+All Correct
+"""
+ 
+ 
+ 
+"""
+Warmup-2 > string_bits
+prev  |  next  |  chance
+Given a string, return a new string made of every other char starting with the first, so "Hello" yields "Hlo".
+
+
+string_bits('Hello') → 'Hlo'
+string_bits('Hi') → 'H'
+string_bits('Heeololeo') → 'Hello'
+"""
+
+def string_bits(str):
+  tamanho = len(str);
+  return str[0:tamanho:2]
+
+# Solution:
+def string_bits(str):
+  result = ""
+  # Many ways to do this. This uses the standard loop of i on every char,
+  # and inside the loop skips the odd index values.
+  for i in range(len(str)):
+    if i % 2 == 0:
+      result = result + str[i]
+  return result
+ 
+ """
+ Expected	Run		
+string_bits('Hello') → 'Hlo'	'Hlo'	OK	
+string_bits('Hi') → 'H'	'H'	OK	
+string_bits('Heeololeo') → 'Hello'	'Hello'	OK	
+string_bits('HiHiHi') → 'HHH'	'HHH'	OK	
+string_bits('') → ''	''	OK	
+string_bits('Greetings') → 'Getns'	'Getns'	OK	
+string_bits('Chocoate') → 'Coot'	'Coot'	OK	
+string_bits('pi') → 'p'	'p'	OK	
+string_bits('Hello Kitten') → 'HloKte'	'HloKte'	OK	
+string_bits('hxaxpxpxy') → 'happy'	'happy'	OK	
+
+All Correct
+ """
