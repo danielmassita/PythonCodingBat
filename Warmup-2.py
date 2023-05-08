2e71fba94d82bf725da0f5bd705369b9ec17a950
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-05-08 14:51:48 -0300

    Update Warmup-2.py

--- Warmup-2.py
+++ Warmup-2.py
@@ -300,3 +300,52 @@ array_count9([9, 2, 4, 3, 1]) → 1	1	OK
 
 All Correct
  """
+ 
+"""
+Warmup-2 > array_front9
+prev  |  next  |  chance
+Given an array of ints, return True if one of the first 4 elements in the array is a 9. The array length may be less than 4.
+
+
+array_front9([1, 2, 9, 3, 4]) → True
+array_front9([1, 2, 3, 4, 9]) → False
+array_front9([1, 2, 3, 4, 5]) → False
+"""
+
+# O Amigo disse...
+def array_front9(nums):
+    end = min(len(nums), 4)
+    for i in range(end):
+        if nums[i] == 9:
+            return True
+    return False
+
+# CodingBat Solution:
+def array_front9(nums):
+  # First figure the end for the loop
+  end = len(nums)
+  if end > 4:
+    end = 4
+  
+  for i in range(end):  # loop over index [0, 1, 2, 3]
+    if nums[i] == 9:
+      return True
+  return False
+ 
+"""
+Expected	Run		
+array_front9([1, 2, 9, 3, 4]) → True	True	OK	
+array_front9([1, 2, 3, 4, 9]) → False	False	OK	
+array_front9([1, 2, 3, 4, 5]) → False	False	OK	
+array_front9([9, 2, 3]) → True	True	OK	
+array_front9([1, 9, 9]) → True	True	OK	
+array_front9([1, 2, 3]) → False	False	OK	
+array_front9([1, 9]) → True	True	OK	
+array_front9([5, 5]) → False	False	OK	
+array_front9([2]) → False	False	OK	
+array_front9([9]) → True	True	OK	
+array_front9([]) → False	False	OK	
+array_front9([3, 9, 2, 3, 3]) → True	True	OK	
+
+All Correct
+"""
