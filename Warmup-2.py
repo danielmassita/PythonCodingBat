d27cae2df3413ffe06b93afd3786ccbb73bd72ab
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-05-07 22:10:02 -0300

    Update Warmup-2.py

--- Warmup-2.py
+++ Warmup-2.py
@@ -167,3 +167,35 @@ str[0:2]
 str[:i]
 """
 
+def string_splosion(str):
+ tamanho = len(str)
+ result = ""
+ for i in range(tamanho+1):
+  strings = str[:i]
+  result += strings
+ return ressult
+
+# Solution:
+def string_splosion(str):
+  result = ""
+  # On each iteration, add the substring of the chars 0..i
+  for i in range(len(str)):
+    result = result + str[:i+1]
+  return result
+
+"""
+Expected	Run		
+string_splosion('Code') → 'CCoCodCode'	'CCoCodCode'	OK	
+string_splosion('abc') → 'aababc'	'aababc'	OK	
+string_splosion('ab') → 'aab'	'aab'	OK	
+string_splosion('x') → 'x'	'x'	OK	
+string_splosion('fade') → 'ffafadfade'	'ffafadfade'	OK	
+string_splosion('There') → 'TThTheTherThere'	'TThTheTherThere'	OK	
+string_splosion('Kitten') → 'KKiKitKittKitteKitten'	'KKiKitKittKitteKitten'	OK	
+string_splosion('Bye') → 'BByBye'	'BByBye'	OK	
+string_splosion('Good') → 'GGoGooGood'	'GGoGooGood'	OK	
+string_splosion('Bad') → 'BBaBad'	'BBaBad'	OK	
+
+All Correct
+"""
+
