2132b101202349cf24695f3d3ebc350020f81641
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-05-08 14:45:06 -0300

    Update Warmup-2.py

--- Warmup-2.py
+++ Warmup-2.py
@@ -251,8 +251,8 @@ def last2(str):
 
   return count
  
- """
- Expected	Run		
+"""
+Expected	Run		
 last2('hixxhi') → 1	1	OK	
 last2('xaxxaxaxx') → 1	1	OK	
 last2('axxxaaxx') → 2	2	OK	
@@ -266,6 +266,37 @@ last2('1717171') → 2	2	OK
 last2('hi') → 0	0	OK	
 last2('h') → 0	0	OK	
 last2('') → 0	0	OK	
+
+All Correct
+"""
+
+"""
+Warmup-2 > array_count9
+prev  |  next  |  chance
+Given an array of ints, return the number of 9's in the array.
+
+
+array_count9([1, 2, 9]) → 1
+array_count9([1, 9, 9]) → 2
+array_count9([1, 9, 9, 3, 9]) → 3
+"""
+
+def array_count9(nums):
+  count = 0
+  for num in nums:
+    if num == 9:
+      count += 1
+  return count 
+ 
+ """
+ Expected	Run		
+array_count9([1, 2, 9]) → 1	1	OK	
+array_count9([1, 9, 9]) → 2	2	OK	
+array_count9([1, 9, 9, 3, 9]) → 3	3	OK	
+array_count9([1, 2, 3]) → 0	0	OK	
+array_count9([]) → 0	0	OK	
+array_count9([4, 2, 4, 3, 1]) → 0	0	OK	
+array_count9([9, 2, 4, 3, 1]) → 1	1	OK	
 
 All Correct
  """
