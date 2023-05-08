e7ed4a7e4b1560008b9faddef2d3f05a4d0a5bbb
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-05-08 15:00:38 -0300

    Update Warmup-2.py

--- Warmup-2.py
+++ Warmup-2.py
@@ -349,3 +349,45 @@ array_front9([3, 9, 2, 3, 3]) → True	True	OK
 
 All Correct
 """
+
+"""
+Warmup-2 > array123
+prev  |  next  |  chance
+Given an array of ints, return True if the sequence of numbers 1, 2, 3 appears in the array somewhere.
+
+
+array123([1, 1, 2, 3, 1]) → True
+array123([1, 1, 2, 4, 1]) → False
+array123([1, 1, 2, 1, 2, 3]) → True
+"""
+
+# O Amigo disse...
+def array123(nums):
+    for i in range(len(nums) - 2):
+        if nums[i] == 1 and nums[i+1] == 2 and nums[i+2] == 3:
+            return True
+    return False
+
+# Solution:
+def array123(nums):
+  # Note: iterate with length-2, so can use i+1 and i+2 in the loop
+  for i in range(len(nums)-2):
+    if nums[i]==1 and nums[i+1]==2 and nums[i+2]==3:
+      return True
+  return False   
+   
+"""
+Expected	Run		
+array123([1, 1, 2, 3, 1]) → True	True	OK	
+array123([1, 1, 2, 4, 1]) → False	False	OK	
+array123([1, 1, 2, 1, 2, 3]) → True	True	OK	
+array123([1, 1, 2, 1, 2, 1]) → False	False	OK	
+array123([1, 2, 3, 1, 2, 3]) → True	True	OK	
+array123([1, 2, 3]) → True	True	OK	
+array123([1, 1, 1]) → False	False	OK	
+array123([1, 2]) → False	False	OK	
+array123([1]) → False	False	OK	
+array123([]) → False	False	OK	
+
+All Correct
+"""
