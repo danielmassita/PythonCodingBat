a9c32c376922cc9c943ff2261365b0c86ee5cafa
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-05-09 02:06:12 -0300

    Create String-2.py

---
+++ String-2.py
@@ -0,0 +1,8 @@
+"""
+String-2 chance
+Medium python string problems -- 1 loop.. Use + to combine strings, len(str) is the number of chars in a String, str[i:j] extracts the substring starting at index i and running up to but not including index j.
+
+double_char H	        count_hi H      	  cat_dog
+count_code        	  end_other H     	  xyz_there
+
+"""
