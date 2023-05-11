7aaf368cb0c9d9bc1735af21e7431e01f3f8e0b6
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-05-10 23:07:06 -0300

    Update String-2.py

--- String-2.py
+++ String-2.py
@@ -141,3 +141,44 @@ OK
 
 All Correct
 """
+
+"""
+String-2 > count_code
+prev  |  next  |  chance
+Return the number of times that the string "code" appears anywhere in the given string, except we'll accept any letter for the 'd', so "cope" and "cooe" count.
+
+
+count_code('aaacodebbb') → 1
+count_code('codexxcode') → 2
+count_code('cozexxcope') → 2
+"""
+
+def count_code(string):
+    count = 0
+    for i in range(len(string) - 3):
+        if string[i:i+2] == 'co' and string[i+3] == 'e':
+            count += 1
+    return count
+  
+"""
+Expected	Run		
+count_code('aaacodebbb') → 1	1	OK	
+count_code('codexxcode') → 2	2	OK	
+count_code('cozexxcope') → 2	2	OK	
+count_code('cozfxxcope') → 1	1	OK	
+count_code('xxcozeyycop') → 1	1	OK	
+count_code('cozcop') → 0	0	OK	
+count_code('abcxyz') → 0	0	OK	
+count_code('code') → 1	1	OK	
+count_code('ode') → 0	0	OK	
+count_code('c') → 0	0	OK	
+count_code('') → 0	0	OK	
+count_code('AAcodeBBcoleCCccoreDD') → 3	3	OK	
+count_code('AAcodeBBcoleCCccorfDD') → 2	2	OK	
+count_code('coAcodeBcoleccoreDD') → 3	3	OK	
+other tests
+OK	
+
+All Correct
+"""
+  
