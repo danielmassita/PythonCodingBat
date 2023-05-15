5b5ee8516179512cfd44900d60526318aeb31e3c
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-05-15 20:33:39 -0300

    Update Logic-2.py

--- Logic-2.py
+++ Logic-2.py
@@ -129,6 +129,51 @@ All Correct
 
 
 
+"""
+Logic-2 > lucky_sum
+prev  |  next  |  chance
+Given 3 int values, a b c, return their sum. However, if one of the values is 13 then it does not count towards the sum and values to its right do not count. So for example, if b is 13, then both b and c do not count.
 
 
+lucky_sum(1, 2, 3) → 6
+lucky_sum(1, 2, 13) → 3
+lucky_sum(1, 13, 3) → 1
+"""
+# Da amiga Vesper...
+def lucky_sum(a, b, c):
+ if a == 13:
+  return 0
+ if b == 13:
+  return a
+ if c == 13:
+  return a + b
+ else:
+  return a + b + c
+ 
+# Da amiga Gio...def lucky_sum(a, b, c):
+  sum = 0
+  for n in (a, b, c):
+    if n != 13:
+      sum += n
+    else:
+      break
+  return sum
+"""
+Expected	Run		
+lucky_sum(1, 2, 3) → 6	6	OK	
+lucky_sum(1, 2, 13) → 3	3	OK	
+lucky_sum(1, 13, 3) → 1	1	OK	
+lucky_sum(1, 13, 13) → 1	1	OK	
+lucky_sum(6, 5, 2) → 13	13	OK	
+lucky_sum(13, 2, 3) → 0	0	OK	
+lucky_sum(13, 2, 13) → 0	0	OK	
+lucky_sum(13, 13, 2) → 0	0	OK	
+lucky_sum(9, 4, 13) → 13	13	OK	
+lucky_sum(8, 13, 2) → 8	8	OK	
+lucky_sum(7, 2, 1) → 10	10	OK	
+lucky_sum(3, 3, 13) → 6	6	OK	
+other tests
+OK	
 
+All Correct
+"""
