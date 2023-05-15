e97dc0b808a33d9f4bf43ee76a143e819fdb9312
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-05-15 20:52:22 -0300

    Update Logic-2.py

--- Logic-2.py
+++ Logic-2.py
@@ -177,3 +177,50 @@ OK
 
 All Correct
 """
+
+
+
+"""
+
+"""
+# Da amiga Vesper...
+def no_teen_sum(a, b, c):
+  def fix_teen(n):
+      if n in [13, 14, 17, 18, 19]:
+          return 0
+      return n
+
+  return fix_teen(a) + fix_teen(b) + fix_teen(c)
+
+ # Da amiga Gio...
+ 
+ 
+ 
+ """
+ Expected	Run		
+no_teen_sum(1, 2, 3) → 6	6	OK	
+no_teen_sum(2, 13, 1) → 3	3	OK	
+no_teen_sum(2, 1, 14) → 3	3	OK	
+no_teen_sum(2, 1, 15) → 18	18	OK	
+no_teen_sum(2, 1, 16) → 19	19	OK	
+no_teen_sum(2, 1, 17) → 3	3	OK	
+no_teen_sum(17, 1, 2) → 3	3	OK	
+no_teen_sum(2, 15, 2) → 19	19	OK	
+no_teen_sum(16, 17, 18) → 16	16	OK	
+no_teen_sum(17, 18, 19) → 0	0	OK	
+no_teen_sum(15, 16, 1) → 32	32	OK	
+no_teen_sum(15, 15, 19) → 30	30	OK	
+no_teen_sum(15, 19, 16) → 31	31	OK	
+no_teen_sum(5, 17, 18) → 5	5	OK	
+no_teen_sum(17, 18, 16) → 16	16	OK	
+no_teen_sum(17, 19, 18) → 0	0	OK	
+other tests
+OK	
+
+All Correct
+ """
+ 
+ 
+ 
+ """
+ """
