05375b178769201379f187e57802de4d3d57b3dc
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-05-16 01:41:32 -0300

    Update List-2.py

--- List-2.py
+++ List-2.py
@@ -144,5 +144,59 @@ All Correct
 
 
 """
+List-2 > sum13
+prev  |  next  |  chance
+Return the sum of the numbers in the array, returning 0 for an empty array. Except the number 13 is very unlucky, so it does not count and numbers that come immediately after a 13 also do not count.
 
+
+sum13([1, 2, 2, 1]) → 6
+sum13([1, 1]) → 2
+sum13([1, 2, 2, 1, 13]) → 6
+"""
+# Da amiga Vesper...
+def sum13(nums):
+    soma = 0
+    i = 0
+    while i < len(nums):
+        if nums[i] == 13:
+            i += 2  # Pula o número 13 e o próximo número
+            continue
+        soma += nums[i]
+        i += 1
+    return soma
+  
+# Da amiga Gio...def sum13(nums):
+  total = 0
+  skip = False
+  if len(nums) == 0:
+    return 0
+  else:
+    for i in range(len(nums)):
+      if nums[i] == 13:
+        skip = True
+      elif skip:
+        skip = False
+      else:
+        total += nums[i]
+  return total
+"""
+Expected	Run		
+sum13([1, 2, 2, 1]) → 6	6	OK	
+sum13([1, 1]) → 2	2	OK	
+sum13([1, 2, 2, 1, 13]) → 6	6	OK	
+sum13([1, 2, 13, 2, 1, 13]) → 4	4	OK	
+sum13([13, 1, 2, 13, 2, 1, 13]) → 3	3	OK	
+sum13([]) → 0	0	OK	
+sum13([13]) → 0	0	OK	
+sum13([13, 13]) → 0	0	OK	
+sum13([13, 0, 13]) → 0	0	OK	
+sum13([13, 1, 13]) → 0	0	OK	
+sum13([5, 7, 2]) → 14	14	OK	
+sum13([5, 13, 2]) → 5	5	OK	
+sum13([0]) → 0	0	OK	
+sum13([13, 0]) → 0	0	OK	
+other tests
+OK	
+
+All Correct
 """
