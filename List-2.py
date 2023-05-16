33847f8f4ce7c73f5d53e2796d0df5b2602bc6a8
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-05-16 01:47:11 -0300

    Update List-2.py

--- List-2.py
+++ List-2.py
@@ -200,3 +200,62 @@ OK
 
 All Correct
 """
+
+
+
+"""
+List-2 > sum67
+prev  |  next  |  chance
+Return the sum of the numbers in the array, except ignore sections of numbers starting with a 6 and extending to the next 7 (every 6 will be followed by at least one 7). Return 0 for no numbers.
+
+
+sum67([1, 2, 2]) → 5
+sum67([1, 2, 2, 6, 99, 99, 7]) → 5
+sum67([1, 1, 6, 7, 2]) → 4
+"""
+# Da amiga Vesper...
+def sum67(nums):
+  soma = 0
+  ignorar = False
+  for num in nums:
+    if num == 6:
+      ignorar = True
+    elif ignorar and num == 7:
+      ignorar = False
+    elif not ignorar:
+      soma += num
+  return soma
+
+# Da amiga Gio...
+def sum67(nums):
+  total = 0
+  skip = False
+  for i in range(len(nums)):
+    if nums[i] == 6:
+      skip = True
+    elif nums[i] == 7 and skip:
+      skip = False
+    elif not skip:
+      total += nums[i]
+  return total
+"""
+Expected	Run		
+sum67([1, 2, 2]) → 5	5	OK	
+sum67([1, 2, 2, 6, 99, 99, 7]) → 5	5	OK	
+sum67([1, 1, 6, 7, 2]) → 4	4	OK	
+sum67([1, 6, 2, 2, 7, 1, 6, 99, 99, 7]) → 2	2	OK	
+sum67([1, 6, 2, 6, 2, 7, 1, 6, 99, 99, 7]) → 2	2	OK	
+sum67([2, 7, 6, 2, 6, 7, 2, 7]) → 18	18	OK	
+sum67([2, 7, 6, 2, 6, 2, 7]) → 9	9	OK	
+sum67([1, 6, 7, 7]) → 8	8	OK	
+sum67([6, 7, 1, 6, 7, 7]) → 8	8	OK	
+sum67([6, 8, 1, 6, 7]) → 0	0	OK	
+sum67([]) → 0	0	OK	
+sum67([6, 7, 11]) → 11	11	OK	
+sum67([11, 6, 7, 11]) → 22	22	OK	
+sum67([2, 2, 6, 7, 7]) → 11	11	OK	
+other tests
+OK	
+
+All Correct
+"""
