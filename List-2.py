6d245ebecef0a2768f61468cbb9ac9e6ba3f106b
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-05-16 01:23:33 -0300

    Update List-2.py

--- List-2.py
+++ List-2.py
@@ -5,3 +5,13 @@ Medium python list problems -- 1 loop.. Use a[0], a[1], ... to access elements i
   count_evens H     big_diff    centered_average
   sum13             sum67       has22
 """
+"""
+List-2 > count_evens
+prev  |  next  |  chance
+Return the number of even ints in the given array. Note: the % "mod" operator computes the remainder, e.g. 5 % 2 is 1.
+
+
+count_evens([2, 1, 2, 3, 4]) → 3
+count_evens([2, 2, 0]) → 3
+count_evens([1, 3, 5]) → 0
+"""
