407c5cf5b6b84094a8f9c82848a314fcdad2ed57
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-05-16 01:22:10 -0300

    Create List-2.py

---
+++ List-2.py
@@ -0,0 +1,7 @@
+"""
+List-2 chance
+Medium python list problems -- 1 loop.. Use a[0], a[1], ... to access elements in a list, len(a) is the length.
+
+  count_evens H     big_diff    centered_average
+  sum13             sum67       has22
+"""
