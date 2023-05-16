36b0deb9dd366ad1f535035bc9d9a0327bf47bb6
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-05-15 21:43:08 -0300

    Update Logic-2.py

--- Logic-2.py
+++ Logic-2.py
@@ -299,3 +299,50 @@ OK
 
 All Correct
 """
+
+
+
+"""
+Logic-2 > close_far
+prev  |  next  |  chance
+Given three ints, a b c, return True if one of b or c is "close" (differing from a by at most 1), while the other is "far", differing from both other values by 2 or more. Note: abs(num) computes the absolute value of a number.
+
+
+close_far(1, 2, 10) → True
+close_far(1, 2, 3) → False
+close_far(4, 1, 3) → True
+"""
+# Da amiga Vesper...
+def close_far(a, b, c):
+  if (abs(a - b) <= 1 and abs(a - c) >= 2 and abs(b - c) >= 2) or (abs(a - c) <= 1 and abs(a - b) >= 2 and abs(b - c) >= 2):
+      return True
+  else:
+      return False
+
+def close_far(a, b, c):
+ ab_diff = abs(a - b)
+ ac_diff = abs(a - c)
+ bc_diff = abs(b - c)
+ if (ab_diff <= 1 and ac_diff >= 2 and bc_diff >= 2) != (ac_diff <= 1 and ab_diff >= 2 and bc_diff >= 2):
+  return True
+ else:
+  return False
+"""
+Expected	Run		
+close_far(1, 2, 10) → True	True	OK	
+close_far(1, 2, 3) → False	False	OK	
+close_far(4, 1, 3) → True	True	OK	
+close_far(4, 5, 3) → False	False	OK	
+close_far(4, 3, 5) → False	False	OK	
+close_far(-1, 10, 0) → True	True	OK	
+close_far(0, -1, 10) → True	True	OK	
+close_far(10, 10, 8) → True	True	OK	
+close_far(10, 8, 9) → False	False	OK	
+close_far(8, 9, 10) → False	False	OK	
+close_far(8, 9, 7) → False	False	OK	
+close_far(8, 6, 9) → True	True	OK	
+other tests
+OK	
+
+All Correct
+"""
